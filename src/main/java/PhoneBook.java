84eaff7969b2cb555607d559cd40cc370c1b7538
Madina Karakotova <vcelennayamoya@gmail.com>
2023-02-27 22:56:36 +0300

    dd implemented.

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,15 @@
-public class PhoneBook {
-static String add(){
-    return  null;
-}
+import java.util.Map;
+import java.util.TreeMap;
 
+public class PhoneBook {
+    static Map<String, Integer> phoneBook = new TreeMap<>();
+    public static Map<String, Integer> getPhoneBook() {
+        return phoneBook;
+    }
+    public static int add(String name, int phoneNumber) {
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.putIfAbsent(name, phoneNumber);
+        }
+        return phoneBook.size();
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 import java.util.TreeMap;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class PhoneBookTest {
 
-    PhoneBook phoneBook = new PhoneBook();
     Map<String, Integer> expected = new TreeMap<>();
 
     @Test
@@ -16,6 +15,9 @@ class PhoneBookTest {
         PhoneBook.add("София", 1234567891);
         PhoneBook.add("Петя", 1234567892);
         int actualNumber = PhoneBook.add("Петя", 1234567892);
+
+        expected.put("Петя", 1234567892);
+        expected.put("София", 1234567891);
         int expectedNumber = expected.size();
 
         Map<String, Integer> actual = PhoneBook.getPhoneBook();
